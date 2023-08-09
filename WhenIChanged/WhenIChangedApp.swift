f73731a89ece73bd8a7452d576b653bd6182ee71
Joel Storr <joel.storr@ewe.net>
2023-08-09 11:30:25 +0200

    Initial Commit

---
+++ WhenIChanged/WhenIChangedApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhenIChangedApp.swift
+//  WhenIChanged
+//
+//  Created by Joel Storr on 09.08.23.
+//
+
+import SwiftUI
+
+@main
+struct WhenIChangedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
