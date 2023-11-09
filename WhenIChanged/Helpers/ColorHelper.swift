e155cb1a71669606aed056a816ce523d92613049
Joel Storr <joel.storr@ewe.net>
2023-11-09 14:30:06 +0100

    Added Color helper to convert core data String to color

--- WhenIChanged/Helpers/ColorHelper.swift
+++ WhenIChanged/Helpers/ColorHelper.swift
@@ -5,4 +5,21 @@
 //  Created by Joel Storr on 09.11.23.
 //
 
-import Foundation
+import SwiftUI
+
+
+enum CardColor:String {
+    case red, green, blue
+}
+
+
+func cardColorConverter(color: CardColor) -> Color{
+    switch color {
+    case .red:
+        return Color.red
+    case .green:
+        return Color.green
+    case .blue:
+        return Color.blue
+    }
+}
