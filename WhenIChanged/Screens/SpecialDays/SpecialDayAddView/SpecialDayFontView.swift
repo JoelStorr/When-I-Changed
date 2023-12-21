757d95e28d8daeb1d86d96e536076e54b0ea2d89
Joel Storr <joel.storr@ewe.net>
2023-12-21 14:54:49 +0100

    Added list of fonts

--- WhenIChanged/Screens/SpecialDays/SpecialDayAddView/SpecialDayFontView.swift
+++ WhenIChanged/Screens/SpecialDays/SpecialDayAddView/SpecialDayFontView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct SpecialDayFontView: View {
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 100))
+    ]
+    
+    let fonts = ["AmericanTypewriter", "ArialMT","Baskerville", "Chalkboard SE", "Chalkduster", "Didot", "Futura", "Georgia", "Hoefler Text", "Marker Felt", "Menlo", "Symbol", "Times New Roman" ]
+    
+    
     var body: some View {
-        Text("Font")
+        
+        LazyVGrid(columns: columns) {
+            
+            
+            ForEach(fonts, id: \.self) { font in
+                
+                Text("Demo Text")
+                    .font(.custom(font, size: 20.0))
+                
+            }
+            
+        }
+        
     }
 }
 
