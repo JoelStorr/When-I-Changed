ce60af232e0983aca3dd2137f4ad6db9348dc472
Joel Storr <joel.storr@ewe.net>
2023-12-28 22:38:52 +0100

    Added special day helper

---
+++ WhenIChanged/Helpers/SpecialDay-CoreDataHelper.swift
@@ -0,0 +1,61 @@
+//
+//  SpecialDay-CoreDataHelper.swift
+//  WhenIChanged
+//
+//  Created by Joel Storr on 28.12.23.
+//
+
+import Foundation
+
+
+extension SpecialDay {
+    
+    var specialDayID: String {
+        get {id?.uuidString ?? "" }
+    }
+    
+    var specialDayName: String {
+        get { name ?? "" }
+        set { name = newValue }
+    }
+    
+    var specialDateColor: String {
+        get { color ?? "red"}
+        set { color = newValue }
+    }
+    
+    var specialDayDate: Date {
+        get { date ?? .now }
+        set { date = newValue }
+    }
+    
+    var specialDayDateToggle: Bool {
+        get { dateToggle }
+        set { dateToggle = newValue }
+    }
+    
+    var specialDayFont: String {
+        get { font ?? "SF Pro" }
+        set { font = newValue }
+    }
+    
+    var specialDayImage: Data {
+        get { image ?? Data() }
+        set { image = newValue }
+    }
+    
+    var specialDayPosition: Int {
+        get { Int(position) }
+        set { position = Int16(newValue) }
+    }
+    
+    var specialDayRepeatNextYear: Bool {
+        get { repeatNextYear }
+        set { repeatNextYear = newValue }
+    }
+    
+    var specialDayWidgetSize: String {
+        get { widgetSize ?? "smal" }
+        set { widgetSize = newValue }
+    }
+}
